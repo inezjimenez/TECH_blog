de7b0f3ed7d17fddeb8b77daa03ca2c8ec0241fb
Inez Jimenez <ijim9182@yahoo.com>
2022-10-13 10:22:37 -0700

    Manually installed dependencies

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,17 +1,51 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
+// const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     User.findAll({
-        attributes: { exclude: ['password']}
+        attributes: { exclude: ['password'] }
     })
-    .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        })
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_body', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                  model: Post,
+                  attributes: ['title']
+                }
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData){
+            res.status(404).json({ message: 'No user found with this id' })
+            return;
+        }
+        res.json(dbUserData)
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err)
     })
 });
-
 
 router.post('/', (req, res) => {
     User.create({
@@ -24,6 +58,7 @@ router.post('/', (req, res) => {
             req.session.user_id = dbUserData.id
             req.session.username = dbUserData.username
             req.session.loggedIn = true
+
             res.json(dbUserData)
         })
     })
@@ -40,21 +75,29 @@ router.post('/login', (req, res) => {
         }
     }).then(dbUserData => {
         if(!dbUserData) {
-            res.status(400).json({ message: 'No user with that email address.'})
+            res.status(400).json({ message: 'No user with that email address!'})
             return;
         }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if(!validPassword){
+            res.status(400).json({ message: 'Incorrect Password!' })
+            return;
+        }
+
         req.session.save(() => {
             req.session.user_id = dbUserData.id;
             req.session.username = dbUserData.username;
             req.session.loggedIn = true;
-            res.json({ user: dbUserData, message: 'You are now logged in.'})
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' })
         })
     })
 })
 
 router.post('/logout', (req, res) => {
     if(req.session.loggedIn) {
-        res.session.destroy(() => {
+        req.session.destroy(() => {
             res.status(204).end();
         })
     }
